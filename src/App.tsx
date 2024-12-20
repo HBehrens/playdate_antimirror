1d1dde5d4f45616994446283cfb1c5eb8fb0704c
Heiko Behrens <mail@HeikoBehrens.net>
2024-12-20 12:33:31 +0100

    capturing content and process to b/w 400x240px

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import VideoCapture from "./VideoCapture.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <VideoCapture/>
+            <br/>
+            <a href={"https://www.notion.so/memfault/Streaming-Pixels-to-Playdate-15e95bc84293811fb645f158237ac4f2"}>🪞
+                Playdate AntiMirror</a> – Heiko's MAD project 2024-12-20
+        </>
+    )
 }
 
 export default App
---
+++ src/VideoCapture.tsx
@@ -0,0 +1,129 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture#options_and_constraints
+import {useEffect, useRef, useState} from "react";
+import "./VideoCapture.css";
+
+async function startCapture(videoElem: HTMLVideoElement) {
+    const displayMediaOptions: DisplayMediaStreamOptions = {
+        video: {
+
+            displaySurface: "window",
+        },
+        audio: false,
+    };
+    const r = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    console.log("start capture", r);
+    videoElem.srcObject = r;
+    return true;
+}
+
+function stopCapture(videoElem: HTMLVideoElement) {
+    const mediaStream = videoElem.srcObject;
+    videoElem.srcObject = null;
+
+    if (!(mediaStream instanceof MediaStream)) {
+        return;
+    }
+    const tracks = mediaStream.getTracks();
+    tracks.forEach((track) => track.stop());
+}
+
+interface CaptureConfig {
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+}
+
+// empirically
+const MACOS_PLAYDATE_SIMULATOR: CaptureConfig = {
+    x: 16,
+    y: 15,
+    w: 400,
+    h: 240,
+};
+
+async function processFrame(videoElem: HTMLVideoElement, canvasElem: HTMLCanvasElement, captureConfig: CaptureConfig) {
+    const {width, height} = canvasElem;
+
+    const ctx = canvasElem.getContext('2d', {willReadFrequently: true});
+    if (!ctx) {
+        return;
+    }
+    ctx.drawImage(videoElem, captureConfig.x, captureConfig.y, captureConfig.w, captureConfig.h, 0, 0, width, height);
+
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const data = imageData.data;
+    console.log(data.length);
+    for (let i = 0; i < data.length; i += 4) {
+        // https://stackoverflow.com/a/52879332
+        const lum = 0.2126 * data[i]
+            + 0.7152 * data[i + 1]
+            + 0.0722 * data[i + 2];
+
+        // for now, this is clamp only
+        const bw = lum > 128 ? 255 : 0;
+
+        data[i] = bw;
+        data[i + 1] = bw;
+        data[i + 2] = bw;
+
+    }
+    ctx.putImageData(imageData, 0, 0);
+    console.log("done");
+}
+
+function VideoCapture() {
+    const [capturing, setCapturing] = useState(false);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [processContinuously, setProcessContinuously] = useState(false);
+
+    const doProcessFrame = async () => {
+        if (videoRef.current && canvasRef.current) {
+            const captureConfig: CaptureConfig = MACOS_PLAYDATE_SIMULATOR;
+            await processFrame(videoRef.current, canvasRef.current, captureConfig);
+        }
+    };
+
+    useEffect(() => {
+        if (processContinuously) {
+            const interval = setInterval(async () => {
+                await doProcessFrame()
+            }, 1000 / 25);
+            return () => clearInterval(interval);
+        } else {
+            return () => {/*no-op*/
+            };
+        }
+    }, [processContinuously]);
+
+    return <div>
+        {capturing ?
+            <button onClick={() => {
+                if (videoRef.current) {
+                    stopCapture(videoRef.current);
+                }
+                setCapturing(false);
+            }}>Stop Capture</button> :
+            <button onClick={async () => {
+                if (videoRef.current) {
+                    setCapturing(await startCapture(videoRef.current));
+                }
+            }}>Start Capture</button>
+        }
+        <video ref={videoRef} id="video" autoPlay></video>
+        <br/>
+        <button onClick={async () => {
+            await doProcessFrame()
+        }}>Process Single Frame
+        </button>
+        <label>
+            <input type="checkbox" checked={processContinuously}
+                   onChange={() => setProcessContinuously(!processContinuously)}/>
+            Process Continuously
+        </label>
+        <canvas ref={canvasRef} width={400} height={240}></canvas>
+    </div>;
+}
+
+export default VideoCapture;
